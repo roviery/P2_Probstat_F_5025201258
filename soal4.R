a80e5839b087c292dae3476b56a2741938a062f4
Nathanael Roviery <roviery@gmail.com>
2022-05-28 16:36:29 +0700

    soal4

---
+++ soal4.R
@@ -0,0 +1,14 @@
+library("ggpubr")
+
+my_data <- read.delim("D:/onewayanova.txt",
+                 header=TRUE)
+
+ggboxplot(my_data, x = "Group", y = "Length",
+          color = "Group", palette = c("#00AFBB", "#E7B800", "#FC4E07"),
+          order = c("1", "2", "3"),
+          ylab = "Length", xlab = "Grup")
+
+model1 <- aov(Length~factor(Group), data=my_data)
+summary(model1)
+
+TukeyHSD(model1)
